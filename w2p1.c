516e5ec74655dbc5c03670695056f1d66e3993e9
AbeTaichi <t246608@st.yamagata-u.ac.jp>
2025-10-10 19:06:47 +0900

    Add files via upload

---
+++ w2p1.c
@@ -0,0 +1,15 @@
+/*Mc*/
+
+#include <stdio.h>
+
+int main()
+{
+    /*変数*/
+    int a = 64;
+    printf("%d Hello Works \n",a);
+    a = a/2;
+    /*printfをつかう*/
+    printf("%d Hello Works ",a);
+
+    return 0;
+}
